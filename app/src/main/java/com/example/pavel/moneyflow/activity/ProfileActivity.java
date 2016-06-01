541901e31fe5629a830269fe6abee7435a858344
oracle <stavr@bk.ru>
2016-06-01 20:49:00 +0300

    onCreate()

--- app/src/main/java/com/example/pavel/moneyflow/activity/ProfileActivity.java
+++ app/src/main/java/com/example/pavel/moneyflow/activity/ProfileActivity.java
@@ -16,17 +16,22 @@ import com.example.pavel.moneyflow.R;
 
 public class ProfileActivity extends AppCompatPreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener  {
 
+    String summary;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //addPreferencesFromResource(R.xml.profile);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
 
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         EditTextPreference editTextPreference = (EditTextPreference) findPreference(key);
+        summary = sharedPreferences.getString(key, getResources().getString(R.string.sumary_login_pref));
+
     }
 
     public static class MyPreferenceFragment extends PreferenceFragment {
