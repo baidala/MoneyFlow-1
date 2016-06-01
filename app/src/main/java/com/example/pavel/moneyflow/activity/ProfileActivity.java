90502f640eb67cd00894cec1cfea37400f78a8f7
oracle <stavr@bk.ru>
2016-06-01 20:52:40 +0300

    PrefFragment implements ChangeListener

--- app/src/main/java/com/example/pavel/moneyflow/activity/ProfileActivity.java
+++ app/src/main/java/com/example/pavel/moneyflow/activity/ProfileActivity.java
@@ -14,31 +14,37 @@ import com.example.pavel.moneyflow.R;
 
 
 
-public class ProfileActivity extends AppCompatPreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener  {
+public class ProfileActivity extends AppCompatPreferenceActivity   {
 
-    String summary;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //addPreferencesFromResource(R.xml.profile);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
 
     }
 
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        EditTextPreference editTextPreference = (EditTextPreference) findPreference(key);
-        summary = sharedPreferences.getString(key, getResources().getString(R.string.sumary_login_pref));
 
-    }
 
-    public static class MyPreferenceFragment extends PreferenceFragment {
+    public static class MyPreferenceFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+        String summary;
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.profile);
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            EditTextPreference editTextPreference = (EditTextPreference) findPreference(key);
+            summary = sharedPreferences.getString(key, getResources().getString(R.string.sumary_login_pref));
+
         }
     }
 
