37c8cb5cd268fde0adbae18e16f9da3215546bf6
oracle <stavr@bk.ru>
2016-06-03 19:21:17 +0300

    add PagerAdapter

---
+++ app/src/main/java/com/example/pavel/moneyflow/adapters/DashboardPagerAdapter.java
@@ -0,0 +1,20 @@
+package com.example.pavel.moneyflow.adapters;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+
+/**
+ * Created by oracle on 6/3/16.
+ */
+public class DashboardPagerAdapter extends PagerAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return false;
+    }
+}
