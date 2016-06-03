34ab4e12f1257392e2cdddcd5c3ee795f86b041b
oracle <stavr@bk.ru>
2016-06-03 19:56:51 +0300

    add PagerAdapter

--- app/src/main/java/com/example/pavel/moneyflow/adapters/DashboardPagerAdapter.java
+++ app/src/main/java/com/example/pavel/moneyflow/adapters/DashboardPagerAdapter.java
@@ -1,20 +1,87 @@
 package com.example.pavel.moneyflow.adapters;
 
-import android.support.v4.view.PagerAdapter;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.example.pavel.moneyflow.R;
 
 /**
  * Created by oracle on 6/3/16.
  */
-public class DashboardPagerAdapter extends PagerAdapter {
+public class DashboardPagerAdapter extends FragmentPagerAdapter {
 
-    @Override
-    public int getCount() {
-        return 0;
+    Context context;
+    public DashboardPagerAdapter(Context context, FragmentManager fm ) {
+        super(fm);
+        this.context = context;
     }
 
     @Override
-    public boolean isViewFromObject(View view, Object object) {
-        return false;
+    public Fragment getItem(int position) {
+        DefaultFragment defaultFragment;
+        Bundle argBundle;
+
+        switch (position) {
+            case 0:
+                defaultFragment = new DefaultFragment();
+                argBundle = new Bundle();
+                argBundle.putString(DefaultFragment.KEY_NAME, R.strings.title_tab_cash_flow);
+                defaultFragment.setArguments(argBundle);
+                return defaultFragment;
+
+            case 1:
+
+                break;
+            case 2:
+                defaultFragment = new DefaultFragment();
+                argBundle = new Bundle();
+                argBundle.putString(DefaultFragment.KEY_NAME, R.strings.title_tab_incomes);
+                defaultFragment.setArguments(argBundle);
+                return defaultFragment;
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return context.getResources().getString(R.string.title_tab_cash_flow);
+            case 1:
+                return context.getResources().getString(R.string.title_tab_expences);
+            case 2:
+                return context.getResources().getString(R.string.title_tab_incomes);
+        }
+        return super.getPageTitle(position);
+    }
+
+    public static class DefaultFragment extends Fragment {
+        static String KEY_NAME = "";
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            String  name = getArguments().getString(KEY_NAME);
+
+            View view = inflater.inflate(android.R.layout.simple_list_item_1, container, false);
+            ((TextView)view.findViewById(android.R.id.text1)).setText(name);
+
+            return super.onCreateView(inflater, container, savedInstanceState);
+        }
     }
 }
